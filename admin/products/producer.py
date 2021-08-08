f81314dbcd8ab407659ca89aa5e890c4a7d3895c
David Koleckar <dkoleckar@gmail.com>
2021-08-08 19:31:47 +0200

    Created python backend

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+import pika, json
+
+params = pika.URLParameters('amqps://iojxyujl:EyPOy4uIHvGbbjEmwW-a3u_4Bl7Wasoj@elk.rmq2.cloudamqp.com/iojxyujl')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
